3bf1d907e6b2472dbd3fcbcab5813f26f0886d98
desz <dg8wiiliams@gmail.com>
2024-06-20 18:54:23 +0200

    SLF4j bumped

--- src/main/java/org/desz/numbertoword/factory/ErrorFactory.java
+++ src/main/java/org/desz/numbertoword/factory/ErrorFactory.java
@@ -8,6 +8,7 @@ import static org.apache.commons.lang3.StringUtils.EMPTY;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.desz.numbertoword.exceptions.AppConversionException;
 import org.desz.numbertoword.language.ProvLang;
 import org.desz.numbertoword.language.ProvLangValues.DeError;
 import org.desz.numbertoword.language.ProvLangValues.FrError;
@@ -22,27 +23,19 @@ public class ErrorFactory {
 	private static String enCacheErrors(ProvLang provLang, String key) {
 
 		switch (provLang) {
-		case UK:
-			ERROR_CACHE.put(ProvLang.UK, of(UkError.values()).collect(toMap(UkError::name, UkError::getError)));
+			case UK -> ERROR_CACHE.put(ProvLang.UK, of(UkError.values())
+					.collect(toMap(UkError::name, UkError::getError)));
 
-			break;
-		case FR:
-			ERROR_CACHE.put(ProvLang.FR, of(FrError.values()).collect(toMap(FrError::name, FrError::getError)));
+			case FR -> ERROR_CACHE.put(ProvLang.FR, of(FrError.values())
+					.collect(toMap(FrError::name, FrError::getError)));
 
-			break;
+			case DE -> ERROR_CACHE.put(ProvLang.DE, of(DeError.values())
+					.collect(toMap(DeError::name, DeError::getError)));
 
-		case DE:
-			ERROR_CACHE.put(ProvLang.DE, of(DeError.values()).collect(toMap(DeError::name, DeError::getError)));
+			case NL -> ERROR_CACHE.put(ProvLang.NL, of(NlError.values())
+					.collect(toMap(NlError::name, NlError::getError)));
 
-			break;
-
-		case NL:
-			ERROR_CACHE.put(ProvLang.NL, of(NlError.values()).collect(toMap(NlError::name, NlError::getError)));
-
-			break;
-
-		default:
-			break;
+			default -> throw new AppConversionException();
 
 		}
 		return ERROR_CACHE.get(provLang).get(key);
@@ -50,15 +43,19 @@ public class ErrorFactory {
 
 	/**
 	 * 
-	 * @param provLang the ProvLang.
-	 * @param key      the key for the error.
+	 * @param provLang
+	 *            the ProvLang.
+	 * @param key
+	 *            the key for the error.
 	 * @return the error mapped to (provLang) key.
 	 */
 	public static String getErrorForProvLang(ProvLang provLang, String key) {
 		if (provLang.equals(ProvLang.EMPTY))
 			return EMPTY;
 
-		return !ERROR_CACHE.containsKey(provLang) ? enCacheErrors(provLang, key) : ERROR_CACHE.get(provLang).get(key);
+		return !ERROR_CACHE.containsKey(provLang)
+				? enCacheErrors(provLang, key)
+				: ERROR_CACHE.get(provLang).get(key);
 
 	}
 
